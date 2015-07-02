5522217242d2573b60b5b480a34957fa86b47372
Fuzzy <ylfuzzy@gmail.com>
2015-07-03 00:09:37 +0800

    Add all

---
+++ appengine-try-python-django/hello/numpytest.py
@@ -0,0 +1,4 @@
+from numpy import *
+
+def numpytest():
+	return matrix(1)
---
+++ appengine-try-python-django/hello/views.py
@@ -0,0 +1,6 @@
+from django import http
+#from numpytest import *
+
+def home(request):
+    #return http.HttpResponse(matrix(1))
+    return http.HttpResponse('Hello World!')
