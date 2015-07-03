9aad4da76fa454703b13fe866b2ed9af03d465e5
Fuzzy <ylfuzzy@gmail.com>
2015-07-03 22:13:09 +0800

    Sucessfully add templates

--- appengine-sqSTFT-python-django/hello/views.py
+++ appengine-sqSTFT-python-django/hello/views.py
@@ -1,6 +1,20 @@
 from django import http
-from numpytest import *
+from numpy import *
+from datetime import datetime
+from django.shortcuts import render
+import os
 
 def home(request):
-    return http.HttpResponse(matrix(1))
+    path = os.path.dirname(__file__) + '/templates/hello_world.html'
+    return render(request,
+                  path,
+                  {'current_time': datetime.now()})
+    #print('first:')
+    #print( __file__ )
+    #print('second:')
+    #print(os.path.dirname(__file__))
+    #print('third:')
+    #print(os.path.dirname('/some/dir/test.py'))
+    #m = [__file__]#, os.path.dirname(__file__)]
+    #return http.HttpResponse(os.path.dirname(__file__) + '/templates/hello_world.html')
     #return http.HttpResponse('Hello World!')
